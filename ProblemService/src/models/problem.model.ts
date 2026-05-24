e73f8f97f487520e772a7fbf808f1186f2752988
saisathwik22 <reddysaisathwik022@gmail.com>
2026-05-24 08:40:05 +0530

    Added basic mongo setup with problem model

---
+++ ProblemService/src/models/problem.model.ts
@@ -0,0 +1,67 @@
+import mongoose, { Document } from "mongoose";
+
+export interface ITestCase {
+    input: string;
+    output: string;
+}
+
+export interface IProblem extends Document {
+    title: string;
+    description: string;
+    difficulty: "easy" | "medium" | "hard";
+    createdAt: Date;
+    updatedAt: Date;
+    editorial?: string;
+    testcases: ITestCase[];
+}
+
+const testSchema = new mongoose.Schema<ITestCase>({
+    input: {
+        type: String,
+        required: [true, "Input is required"],
+        trim: true,
+    },
+    output: {
+        type: String,
+        required: [true, "Output is required"],
+        trim: true,
+
+    }
+}, {
+    // _id: false, // if you dont want each testcase to have own unique id
+})
+
+const problemSchema = new mongoose.Schema<IProblem>({
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+        maxLength: [100, "Title must be less than 100 characters"],
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+        trim: true,
+    },
+    difficulty: {
+        type: String,
+        enum: {
+            values: ["easy", "medium", "hard"],
+            message: "Invalid difficulty level",
+        },
+        default: "easy",
+        required: [true, "Difficulty level is required"]
+    },
+    editorial: {
+        type: String,
+        trim: true,
+    },
+    testcases: [testSchema],
+}, {
+    timestamps: true
+})
+
+problemSchema.index({ title: 1 }, { unique: true });
+problemSchema.index({ difficulty: 1 }); // index on difficulty field
+
+export const Problem = mongoose.model<IProblem>("Problem", problemSchema)
